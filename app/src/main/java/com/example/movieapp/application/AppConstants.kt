854a923fd44dd4ed7846336e907286a920c251dd
DanielAceves <idi_chop@hotmail.com>
2022-09-26 00:54:23 -0500

    Movie App Finalizada

---
+++ app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.application
+
+object AppConstants {
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "be9af78f5b18ebba5bb782a59d7424dc"
+}
