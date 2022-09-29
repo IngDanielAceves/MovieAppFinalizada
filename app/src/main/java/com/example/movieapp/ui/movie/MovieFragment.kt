6b8d85e6bcec948ee516e299dc6907d2bcbe5451
DanielAceves <idi_chop@hotmail.com>
2022-09-28 23:21:38 -0500

    Agrege network_security_config.xml y ademas Update compiladores y dependencias

--- app/src/main/java/com/example/movieapp/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movie/MovieFragment.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ConcatAdapter
 import com.example.movieapp.R
@@ -38,27 +38,51 @@ class MovieFragment : Fragment(R.layout.fragment_movie), MovieAdapter.OnMovieCli
 
         concatAdapter = ConcatAdapter()
 
-        viewModel.fetchMainScreenMovies().observe(viewLifecycleOwner, Observer { result ->
-            when (result){
+        viewModel.fetchMainScreenMovies().observe(viewLifecycleOwner) { result ->
+            when (result) {
                 is Resource.Loading -> {
                     binding.progressBar.visibility = View.VISIBLE
                 }
                 is Resource.Success -> {
                     binding.progressBar.visibility = View.GONE
                     concatAdapter.apply {
-                        addAdapter(0, UpcomingConcatAdapter(MovieAdapter(result.data.first.results, this@MovieFragment )))
-                        addAdapter(1, TopRatedConcatAdapter(MovieAdapter(result.data.second.results, this@MovieFragment )))
-                        addAdapter(2, PopularConcatAdapter(MovieAdapter(result.data.third.results, this@MovieFragment )))
+                        addAdapter(
+                            0,
+                            UpcomingConcatAdapter(
+                                MovieAdapter(
+                                    result.data.first.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
+                        addAdapter(
+                            1,
+                            TopRatedConcatAdapter(
+                                MovieAdapter(
+                                    result.data.second.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
+                        addAdapter(
+                            2,
+                            PopularConcatAdapter(
+                                MovieAdapter(
+                                    result.data.third.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
                     }
 
                     binding.rvMovies.adapter = concatAdapter
                 }
                 is Resource.Failure -> {
                     binding.progressBar.visibility = View.GONE
-                    Log.d("Error","${result.exception}")
+                    Log.d("Error", "${result.exception}")
                 }
             }
-        })
+        }
     }
 
     override fun onMovieClick(movie: Movie) {
